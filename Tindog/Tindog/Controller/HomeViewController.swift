1b000479045cccea3480d62ee3531a5bff66a0cc
jean <jean@jeans-Mac.local>
2018-03-12 21:19:35 -0700

    adding control gestures

---
+++ Tindog/Tindog/Controller/HomeViewController.swift
@@ -0,0 +1,62 @@
+//
+//  HomeViewController.swift
+//  Tindog
+//
+//  Created by jean on 3/12/18.
+//  Copyright © 2018 jean. All rights reserved.
+//
+
+import UIKit
+
+class NavigationImageView: UIImageView {
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return CGSize(width: 76, height:39)
+    }
+}
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var cardView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let titleView = NavigationImageView()
+        titleView.image = UIImage(named: "Actions")
+        self.navigationItem.titleView = titleView
+        let homeGR = UIPanGestureRecognizer(target: self, action: #selector(cardDragged(gestureRecognizer:)))
+        self.cardView.addGestureRecognizer(homeGR)
+        
+    }
+    
+    @objc func cardDragged(gestureRecognizer: UIPanGestureRecognizer){
+        //print("Drag \(gestureRecognizer.translation(in: view))")
+        let cardPoint = gestureRecognizer.translation(in: view)
+        self.cardView.center = CGPoint(x: self.view.bounds.width / 2 + cardPoint.x, y: self.view.bounds.height / 2 + cardPoint.y)
+        if gestureRecognizer.state == .ended{
+            print(self.cardView.center)
+            if self.cardView.center.x < (self.view.bounds.width / 2 - 100) {
+                print("dislike")
+            }
+            if self.cardView.center.x < (self.view.bounds.width / 2 + 100) {
+                print("Like")
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
