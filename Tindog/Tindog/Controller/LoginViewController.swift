b922cf9298c5b25b8df7f0a7b6ca76d5ba4f68a9
jean <jean@jeans-Mac.local>
2018-03-17 23:38:47 -0700

    closebtn

---
+++ Tindog/Tindog/Controller/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  Tindog
+//
+//  Created by jean on 3/17/18.
+//  Copyright © 2018 jean. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.bindKeyboard()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        self.view.addGestureRecognizer(tap)
+        // Do any additional setup after loading the view.
+    }
+
+    func handleTap(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closeBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
